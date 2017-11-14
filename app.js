3eced99868a7238545ae41084c1bfc4307a4aec3
Jules Bonnard <julesbonnard@gmail.com>
2017-11-14 02:18:57 +0100

    remove for exercice

--- app.js
+++ app.js
@@ -16,27 +16,20 @@ var app = new Vue({
     submitButton: function submitButton (answer) {
       // Modifie la propriété succès en fonction de la réponse (QCM)
       var correction = this.step.choices[this.step.answer];
-      if (answer === correction) {
-        this.success = true;
-      } else {
-        this.success = false;
-      }
+
+      // ******* Ecris ici la condition nécessaire
     },
     submitText: function submitText () {
       // Modifie la propriété succès en fonction de la réponse (réponse libre)
       var answer = this.$refs.textInput.value;
       var correction = this.step.answer;
-      if (answer === correction) {
-        this.success = true;
-      } else {
-        this.success = false;
-      }
+
+      // ******* Ecris ici la condition nécessaire
     },
     nextQuestion: function nextQuestion () {
       // Incrémente le score, réinitialise la propriété succès, passe à la question suivante
-      if (this.success) this.score++;
-      this.success = null;
-      this.current++;
+
+      // ******* Ecris ici les actions et la condition nécessaires
     },
     buttonClasses: function buttonClasses (value) {
       // Renvoie les classes pour colorer les bouttons QCM en fonction de la réponse
